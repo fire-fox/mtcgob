96a4e530b49c28ae63347e6ce4beee4bd2fa7379
Erik <erik.manchego@gmail.com>
2022-12-15 21:23:24 -0500

    deleted

--- styles.js
+++
@@ -1,9 +0,0 @@
-function validate(){
-var username = document.getElementById("username").value;
-var password = document.getElementById("password").value;
-if ( username == "mtc" && password == "mtc2022"){
-    window.location="dashboard-1.html"; 
-return false;
-}
-
-}
